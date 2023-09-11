a3e7db402a28439e209cca4f69c0a91dde5dc5f1
belupaez <116501672+belupaez@users.noreply.github.com>
2023-09-11 19:47:31 -0300

    Add files via upload

---
+++ potrero.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<?php
+
+echo "Hola mundo ";
+
+print ("</br>");
+
+$saludo = "Hola mundo";
+
+echo $saludo;
+
+print ("</br>");
+
+$variable1 = 100;
+
+$variable2 = 25;
+
+echo $variable1 + $variable2;
+
+print ("</br>");
+
+echo $variable1 - $variable2;
+
+print ("</br>");
+
+echo $variable1 * $variable2;
+
+print ("</br>");
+
+echo $variable1 / $variable2;
+
+print ("</br>");
+
+echo $variable1 % $variable2;
+
+print ("</br>");
+
+$gradosc = 20;
+
+$gradosf = ($gradosc * 9/5) + 32;
+
+echo $gradosf;
+
+print ("</br>");
+
+$base = 18;
+
+$altura = 12;
+
+$perimetro_rectangulo = 2 * ($base + $altura);
+
+echo $perimetro_rectangulo;
+
+print ("</br>");
+
+$area_rectangulo = $base * $altura;
+
+echo $area_rectangulo;
+
+print ("</br>");
+
+$radio = 30;
+
+$pi = 3.14;
+
+$perimetro_circulo = (2 * $radio) * $pi;
+
+echo $perimetro_circulo;
+
+print ("</br>");
+
+$area_circulo = $pi * $radio ** 2;
+
+echo $area_circulo;
+
+print ("</br>");
+
+ ?>
+
+  </body>
+</html>
